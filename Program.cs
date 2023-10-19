322d9090a3aefe4b442533f0ee8f9527edd2fa6a
Lucas Cardoso <lucasreis-@outlook.com>
2023-10-18 23:03:23 -0300

    tabuleiro e menu de opções

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Desafio_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //JOGO DA VELHA
+            menu();
+            tabuleiro();
+        }
+
+        static void tabuleiro()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("");
+        }
+
+    
+        static void menu()
+        {
+            Console.WriteLine("##########################");
+            Console.WriteLine("##### JOGO DA VELHA ######");
+            Console.WriteLine("##########################");
+            Console.WriteLine("# 1 - Player vs Player   #");
+            Console.WriteLine("# 2 - Player vs Máquina  #");
+            Console.WriteLine("##########################");
+            Console.Write("SELECIONE UMA OPÇÃO: ");
+            string escolha = Console.ReadLine();
+        }
+    }
+}
